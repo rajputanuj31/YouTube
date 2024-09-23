9f595b3f3741dbe3182e141c87d6c1b4bc82b371
Anuj Rajput <rajputanuj041@gmail.com>
2024-09-23 21:19:52 +0530

    Basic utils added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!fs.existsSync(localFilePath)) {
+            throw new Error("File not found");
+        }
+
+        const result = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",
+        });
+
+        console.log("File uploaded successfully", result.secure_url);
+        return result;
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        console.error("Error uploading video to cloudinary", error);
+        throw error;
+    }
+}
+
+export {uploadOnCloudinary}
