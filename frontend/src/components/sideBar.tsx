2eec8533b699ae06238c73b3bc85dc1bac0e7e89
Anuj Rajput <rajputanuj041@gmail.com>
2024-10-14 19:08:00 +0530

    Subscriptions added in sidebar also

--- frontend/src/components/sideBar.tsx
+++ frontend/src/components/sideBar.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
-import { FaHome, FaBolt, FaYoutube, FaBook, FaHistory, FaVideo, FaClock, FaCut, FaChevronDown, FaUser } from 'react-icons/fa';
+import React, { useEffect, useState } from 'react';
+import { FaHome, FaBolt, FaYoutube, FaBook, FaHistory, FaVideo, FaClock, FaCut, FaChevronDown } from 'react-icons/fa';
 import Link from 'next/link';
+import { useSelector } from 'react-redux'
+
+interface Channel {
+  _id: string;
+  channel: string;
+  channelDetails?: {
+    avatar?: string;
+    fullName?: string;
+    username?: string;
+  };
+  totalSubscribers: number;
+  isSubscribed: boolean;
+}
 
 const Sidebar = () => {
+  const [subscribedChannels, setSubscribedChannels] = useState<Channel[]>([]);
+  const currentUser = useSelector((state: any) => state.user.currentUser?.data.user)
+
+  useEffect(() => {
+    const fetchSubscribedChannels = async () => {
+      try {
+        const response = await fetch(`/api/v1/subscription/get-subscribed-channels/${currentUser._id}`)
+        if (!response.ok) {
+          throw new Error('Failed to fetch subscribed channels');
+        }
+        const data = await response.json();
+        setSubscribedChannels(data.data.subscribedChannels);
+        
+      } catch (error) {
+        console.error('Error fetching subscribed channels:', error);
+      }
+    };
+
+    fetchSubscribedChannels();
+  }, []);
+
   return (
-    <nav className="fixed top-16 left-0 h-full w-64 bg-black p-6 text-white z-10">
+    <nav className="fixed top-16 left-0 h-full w-64 bg-black p-6 text-white z-10 border-r border-gray-700 border-opacity-50">
       <ul className="space-y-4">
         <li className="hover:bg-gray-800 p-2 rounded transition-colors">
           <Link href="/" className="flex items-center">
@@ -26,9 +60,20 @@ const Sidebar = () => {
       </ul>
 
       <div className="mt-10">
-        <h3 className="text-sm font-semibold mb-4">Subscriptions</h3>
+        <h3 className="text-sm font-semibold mb-4">
+          <Link href="/subscriptions" className="text-white">
+            Subscriptions
+          </Link>
+        </h3>
         <ul className="space-y-2">
-          <li className="hover:bg-gray-800 p-2 rounded transition-colors flex items-center"><FaUser className="mr-2" /> Andres Vidoza</li>
+          {subscribedChannels.map(channel => (
+            <li key={channel._id} className="hover:bg-gray-800 p-2 rounded transition-colors flex items-center">
+              <img src={channel.channelDetails?.avatar || ''} alt="Channel Avatar" className="mr-2 w-6 h-6 rounded-full" />
+              <Link href={`/profile/${channel.channel}`} className="text-white">
+                {channel.channelDetails?.fullName || 'Unknown Channel'}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </nav>
